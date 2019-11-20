181328e6b14322fe6be151ec1e1b8ac7084e50aa
zeus <wpdn79@likelion.org>
2019-11-20 10:49:58 +0900

    회원가입 페이지 폼 작성

--- MainController.js
+++ MainController.js
@@ -1,3 +1,6 @@
 exports.index = (req, res) => {
-  res.render("client/main");
+  res.render("client/main", { title1: "메인페이지" });
+};
+exports.join = (req, res) => {
+  res.render("client/join", { title1: "회원가입" });
 };
--- index.js
+++ index.js
@@ -2,24 +2,23 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const hpp = require("hpp");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 const app = express();
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: false
-  })
-);
 app.set("view engine", "ejs");
 app.use(morgan());
 app.use(helmet());
 app.use(hpp());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//app.use(localMiddleware);
 
 const MainController = require("./MainController");
 
 app.route("/").get(MainController.index);
+app.route("/join").get(MainController.join);
 
 app.listen(process.env.PORT, () => {
   console.log("listening at 3000 port");
